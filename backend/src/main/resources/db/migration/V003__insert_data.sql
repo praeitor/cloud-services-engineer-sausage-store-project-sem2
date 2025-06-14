6c8f92ff9637160eacca6a9ae5116ea55ccd05a3
Denis Bulgakov <praeitor@gmail.com>
2025-06-14 12:17:43 +0400

    fix orders duplicate key for orders

--- backend/src/main/resources/db/migration/V003__insert_data.sql
+++ backend/src/main/resources/db/migration/V003__insert_data.sql
@@ -9,6 +9,9 @@ INSERT INTO product (id, name, picture_url, price) VALUES
 INSERT INTO orders (id, status, date_created)
     SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW())))
     FROM generate_series(1, 10000) s(i);
+
+-- Сброс последовательности id, чтобы избежать конфликтов при автоинкременте
+SELECT setval('orders_id_seq', (SELECT MAX(id) FROM orders));
     
 INSERT INTO order_product (quantity, order_id, product_id)
     SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6
